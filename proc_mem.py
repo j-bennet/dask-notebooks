7a000e9f687141d5d34f0816aebcc5557cb0a15d
Irina Truong <i.chernyavska@gmail.com>
2023-03-30 14:45:24 -0700

    Fix memory usage per partition.

--- proc_mem.py
+++ proc_mem.py
@@ -7,20 +7,23 @@ from dask.utils import format_bytes
 
 
 if __name__ == "__main__":
-    string_dtype = "string[pyarrow]"
-    num_records = 10000000
+    # string_dtype = "string[pyarrow]"
+    string_dtype = object
+    num_records = 10_000_000
+    num_unique = 1_000_000
+    names = [
+        "".join(random.choices(string.ascii_letters + string.digits + " ", k=random.randint(10, 100)))
+        for _ in range(num_unique)
+    ]
     process = psutil.Process(os.getpid())
     mem1 = process.memory_info().rss
     print(f"{string_dtype = }")
     print(f"before: {format_bytes(mem1)}")
 
-    s = pd.Series([
-        "".join(random.choices(string.ascii_letters + string.digits + " ", k=random.randint(25, 40)))
-        for _ in range(num_records)
-    ], dtype=string_dtype)
+    s = pd.Series([random.choice(names) for _ in range(num_records)], dtype=string_dtype)
 
     mem_s = s.memory_usage(deep=True)
-    print(f"series: {format_bytes(mem_s)}")
-    mem2 = process.memory_info().rss
-    print(f"after: {format_bytes(mem2)}")
-    print(f"difference: {format_bytes(mem2 - mem1)}")
+    print(f"pandas reported: {format_bytes(mem_s)}")
+    mem2 = process.memory_full_info().rss
+    # print(f"after: {format_bytes(mem2)}")
+    print(f"psutil reported: {format_bytes(mem2 - mem1)}")
